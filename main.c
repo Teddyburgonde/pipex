1a08fc73c0003f3850a2ea6ed806824edde844dc
Teddy Bandama <tebandam@1G7.42angouleme.fr>
2024-01-27 14:10:11 +0100

    message error for argc != 5

--- main.c
+++ main.c
@@ -6,9 +6,12 @@
 /*   By: tebandam <tebandam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 13:49:13 by tebandam          #+#    #+#             */
-/*   Updated: 2024/01/27 13:51:12 by tebandam         ###   ########.fr       */
+/*   Updated: 2024/01/27 14:09:31 by tebandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	can_be_opened()
+
 
 int	parsing(int argc)
 {
@@ -19,7 +22,12 @@ int	parsing(int argc)
 	
 }
 
+
 int	main(int argc, char **argv)
 {
-	
+	if (argc != 5)
+	{
+		ft_printf("pipex : ./pipex file1 cmd1 cmd2 file2");
+	}
 }
+
